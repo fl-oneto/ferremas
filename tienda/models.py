f68cb07c50eb36278348a6b3c171c7fa89f5e706
fl-oneto <fl.oneto@duocuc.cl>
2025-05-01 20:30:40 -0400

    se suben categorias y productos, actualizacion de las vistas

--- tienda/models.py
+++ tienda/models.py
@@ -67,7 +67,7 @@ class UnidadMedida(models.Model):
     descripcion = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.nombre
+        return self.unidad_medida
 
 # Tabla Producto
 class Producto(models.Model):
--- tienda/views.py
+++ tienda/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Categoria, Producto, Rol, Usuario, Region, Comuna, Direccion, Telefono, UnidadMedida, Carrito, ItemCarrito, Pedido, DetallePedido, MetodoPago, Pago
 
-def landing(request):
-    return render(request, 'landing_page/index.html')
+def home(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html', {'categorias': categorias})
 
 
